25c98e9baaa231be6342e39ad81f9bf88041ad0d
Jeffrey <jeffreychen846@gmail.com>
2024-06-19 00:42:00 -0400

    Looping

--- src/DieRoller.java
+++ src/DieRoller.java
@@ -2,18 +2,22 @@ import com.sun.source.tree.WhileLoopTree;
 
 import javax.xml.transform.Source;
 import java.util.Random;
+import java.util.Scanner;
 
 public class DieRoller
 {
     public static void main(String[] args) {
         Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+
+
         int dieA = 0;
         int dieB = 0;
         int dieC = 0;
         int numOfRoll = 0;
         int sum;
         String equal = "";
-        String playAgain = "N";
+        String playAgain = "Y";
         do {
             System.out.println("--------------------------------------------------------------------------");
             do {
@@ -35,8 +39,10 @@ public class DieRoller
                 }
             } while (equal.equals("F"));
             System.out.println("--------------------------------------------------------------------------");
-            System.out.println("Type Y to go again.");
-        } while (playAgain.equalsIgnoreCase(""))
+            System.out.println("Type Y to go again. Type any other letter to exit.");
+            playAgain = in.nextLine();
+            numOfRoll = 0;
+        } while (playAgain.equalsIgnoreCase("Y"));
 
     }
 }
